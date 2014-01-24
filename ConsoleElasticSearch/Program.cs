6a7fd0d4ab982a1996ed6f416bbfbf2d4e9a2796
damienbod <damien_bod@hotmail.com>
2014-01-24 09:34:03 +0100

    init

---
+++ ConsoleElasticSearch/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using log4net;
+
+namespace ConsoleElasticSearch
+{
+    class Program
+    {
+        private static readonly ILog _log = LogManager.GetLogger(typeof(Program));
+
+        static void Main(string[] args)
+        {
+            for (int i = 1; i < 100; i++)
+            {
+                _log.Info("console test log " + i);  
+            }
+            
+        }
+    }
+}
